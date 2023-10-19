809fbee6a23e691e97931602c23338e712cdc30c
KingYen <1696918097@qq.com>
2023-10-20 01:04:12 +0800

    Create the home page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
 import Route from './router/route'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Route></Route>
-      </header>
+    <div>
+      <Route></Route>
     </div>
   );
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+import { AiOutlineCode } from "react-icons/ai";
+import { BsGlobe, BsPhoneVibrate } from "react-icons/bs";
+import { CgMenuGridO } from "react-icons/cg";
+
+import logo from "../../Assets/Logo.png";
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className="navBar flex">
+			<div className="navBarOne flex">
+				<div>
+					<AiOutlineCode className="icon" />
+				</div>
+
+				<div className="none flex">
+					<li className="flex">
+						<BsPhoneVibrate className="icon" />
+						Support
+					</li>
+					<li className="flex">
+						<BsGlobe className="icon" />
+						Languages
+					</li>
+				</div>
+
+				<div className="atb flex">
+					<span>Sign In</span>
+					<span>Sign Out</span>
+				</div>
+			</div>
+			<div className="navBarTwo">
+				<div className="logo">
+					<img src={logo} alt="logo" className="logo" />
+				</div>
+			</div>
+
+			<div className="navBarMenu">
+				<ul className="menu flex">
+					<li className="lisItem">Home</li>
+					<li className="lisItem">About</li>
+					<li className="lisItem">Offers</li>
+					<li className="lisItem">Seats</li>
+					<li className="lisItem">Destinations</li>
+				</ul>
+
+				<button className="btn flex btnOne">Contact</button>
+
+				<div className="toggleIcon">
+					<CgMenuGridO className="icon" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
